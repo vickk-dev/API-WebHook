5cf70561753469e3a7d66a04acc83b1ac1294d17
Gabreelz <gabriel_teixeira30@hotmail.com>
2025-10-02 15:18:26 -0300

    SoftwareHouse model

---
+++ scr/Infrastructure/Persistence/Sequelize/models/SoftwareHouse.js
@@ -0,0 +1,44 @@
+module.exports = (Sequelize, DataTypes) => {
+
+    const SoftwareHouse = Sequelize.define("SoftwareHouse", {
+
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        data_criacao: {
+            type: DataTypes.DATA,
+            allownull: false,
+            defaultValue: DataTypes.NOW
+        },
+
+        cnpj: {
+            type: DataTypes.STRING(14),
+            allowNull: false,
+            unique: true,
+            validate: {
+                len: [14, 14],
+                isNumeric: true
+            }
+        },
+
+        token: {
+            type: DataTypes.STRING,
+            alloeNull: false
+        },
+
+        status: {
+            type: Sequelize.ENUM("ativo", "inativo"),
+            allowNull: false
+        }
+
+    }, {
+        tableName: "SoftwareHouse",
+
+        timestamp: false
+    });
+
+    return SoftwareHouse;
+}
