228e306d63408d77b0178702164f07944b9253f5
Gabriel-S-Mendes <mesente12@gmail.com>
2025-10-03 16:36:32 -0300

    adicionar models webhockReprocessado, convenio, servico ver.1.0

---
+++ scr/Infrastructure/Persistence/Sequelize/models/convenio.js
@@ -0,0 +1,50 @@
+// Feito Por Gabriel
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Convenio extends Model {
+    static associate(models) {
+      Convenio.belongsTo(models.Conta, {
+        foreignKey: 'conta_id',
+        as: 'conta'
+      });
+      
+      Convenio.hasMany(models.Servico, {
+        foreignKey: 'convenio_id',
+        as: 'servicos'
+      });
+    }
+  }
+  
+  Convenio.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    numero_convenio: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    data_criacao: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    },
+    conta_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Contas',
+        key: 'id'
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Convenio',
+    tableName: 'Convenios',
+    timestamps: false
+  });
+
+  return Convenio;
+};
