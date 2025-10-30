db4b3423fe5c546ebee0cfeaebbb9aec9f862573
Felip4n <arantes.felip25@gmail.com>
2025-10-30 13:47:28 -0300

    Arrumando App.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const errorHandler = require('./config/middlewares/errorHandler.js');
-const app = express();
-const ReenvioRoutes = require('./Rotas/ReenvioRota.js');
+const ReenvioRoutes = require('./rotas/ReenvioRota.js');
 const ProtocoloRoutes = require('./rotas/ProtocoloRota.js');
+const app = express();
 
 app.use(express.json());
 
 app.use('/api', ReenvioRoutes);
 app.use('/api', ProtocoloRoutes);
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
 app.use(errorHandler);
+
 module.exports = app;
